e89932edd25a99c8e16ad8082fe660bedaaf6c88
Muhammad Asfand Khan <134827741+asfand2315@users.noreply.github.com>
2023-05-29 01:11:00 -0700

    Update Header.js

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import PersonIcon from '@material-ui/icons/Person';
 import '../CSS/header.css';
 const Header = () => {
     return (
@@ -9,23 +13,22 @@ const Header = () => {
   
         <div className="header-navigation">
           <button className="header-menu-button">
-            <MenuIcon />
+            
           </button>
           <div className="header-search-bar">
             <input type="text" placeholder="Search movies..." />
             <button className="header-search-button">
-              <SearchIcon />
+              
             </button>
           </div>
           <button className="header-watchlist-button">
-            <FavoriteIcon />
+            
           </button>
           <button className="header-auth-button">
-            <PersonIcon />
             <span>Sign In / Sign Up</span>
           </button>
         </div>
       </header>
     );
   };
-export default Header;
+export default Headers;

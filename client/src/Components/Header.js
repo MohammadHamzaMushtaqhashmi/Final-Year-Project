b91d175f9789b78598deda6416705a1ad5e00832
Muhammad Asfand Khan <134827741+asfand2315@users.noreply.github.com>
2023-05-29 01:24:25 -0700

    update header

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import PersonIcon from '@material-ui/icons/Person';
 import '../CSS/header.css';
-const Header = () => {
-    return (
-      <header>
-        <div className="header-logo">
-          <img src="/path/to/logo.png" alt="MovieMate" />
-        </div>
-  
-        <div className="header-navigation">
-          <button className="header-menu-button">
-            
-          </button>
-          <div className="header-search-bar">
-            <input type="text" placeholder="Search movies..." />
-            <button className="header-search-button">
-              
-            </button>
-          </div>
-          <button className="header-watchlist-button">
-            
-          </button>
-          <button className="header-auth-button">
-            <span>Sign In / Sign Up</span>
-          </button>
-        </div>
-      </header>
-    );
-  };
-export default Headers;
+
+function Header() {
+  return (
+    <header>
+      <div className="logo">MovieMate</div>
+      <button className="menu-button">Menu</button>
+      <input type="text" placeholder="Search" className="search-bar" />
+      <button className="watchlist-button">Watchlist</button>
+      <button className="sign-in-button">Sign In</button>
+      <button className="sign-up-button">Sign Up</button>
+    </header>
+  );
+}
+
+export default Header;

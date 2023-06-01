f401bac55b792a5050f547e47e108b7bbcf9dbe8
HaMza HashMi <73049209+MohammadHamzaMushtaqhashmi@users.noreply.github.com>
2023-06-01 11:55:50 +0500

    Updated header

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import '../CSS/header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faBookmark, faSearch } from '@fortawesome/free-solid-svg-icons';
+
 function Header() {
   return (
     <header>
-      <img src="/images/logo.jpg" alt="MovieMate Logo" className="logo" />
-      <button className="menu-button">Menu</button>
-      <input type="text" placeholder="Search" className="search-bar" />
-      <button className="watchlist-button">Watchlist</button>
-      <button className="sign-in-button">Sign In</button>
-      <button className="sign-up-button">Sign Up</button>
+      <div className="header-left">
+        <img src="/images/logo.jpg" alt="MovieMate Logo" className="logo" />
+        <button className="menu-button">
+          <FontAwesomeIcon icon={faBars} /> Menu
+        </button>
+      </div>
+      <div className="search-container">
+        <select id="search-select">
+          <option value="all">All</option>
+          <option value="movies">Movies</option>
+          <option value="tv_shows">TV Shows</option>
+          <option value="celebrities">Celebrities</option>
+          <option value="titles">Titles</option>
+        </select>
+        <input type="text" placeholder="Search" className="search-bar" />
+        <button className="search-button">
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </div>
+      <div className="header-right">
+        <button className="watchlist-button">
+          <FontAwesomeIcon icon={faBookmark} /> Watchlist
+        </button>
+        <button className="sign-in-button">Sign In</button>
+        <button className="sign-up-button">Sign Up</button>
+      </div>
     </header>
   );
 }

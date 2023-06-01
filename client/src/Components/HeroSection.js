a195ea321ff71c7bc20958b370ac5b66ee42e5c0
HaMza HashMi <73049209+MohammadHamzaMushtaqhashmi@users.noreply.github.com>
2023-06-01 09:13:31 +0500

    update hero section

--- client/src/Components/HeroSection.js
+++ client/src/Components/HeroSection.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import '../CSS/hero.css'
+function Hero() {
+  const [movies, setMovies] = useState([]);
 
-function HeroSection() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/now_playing?api_key=499d99db6ce23991d21afde0deede0f1`
+        );
+        setMovies(response.data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const handleMovieClick = (movieId) => {
+    // Add code to navigate to a new page with more information about the movie
+    window.location.href = `/movie/${movieId}`;
+  };
+
   return (
     <div className="hero">
-      <img
-        src="/images/cover.jpg"
-        alt="MovieMate"
-        style={{ width: '100%', height: '500px', objectFit: 'cover' }}
-      />
+      <Slider {...settings}>
+        {movies.map((movie) => (
+          <div key={movie.id} onClick={() => handleMovieClick(movie.id)}>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+            <h2>{movie.title}</h2>
+          </div>
+        ))}
+      </Slider>
+      {/* Add other hero section content here */}
     </div>
   );
 }
 
-export default HeroSection;
+export default Hero;
